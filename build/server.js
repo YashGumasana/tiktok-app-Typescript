7ede9f58fab8aab23f8933d84d84ddee1d17c373
YashGumasana <115367100+YashGumasana@users.noreply.github.com>
2023-01-20 19:59:10 +0530

    Update server.js

--- build/server.js
+++ build/server.js
@@ -8,5 +8,3 @@ const port = process.env.PORT || 80;
 src_1.default.listen(port, () => {
     console.log(`server started on port ${port}.....`);
 });
-// "db_url": "mongodb+srv://YashGumasana:9879445325120@nodeexpressprojects.nmkks8r.mongodb.net/SAMPLE_PROJECT_TS"
-//# sourceMappingURL=server.js.map
